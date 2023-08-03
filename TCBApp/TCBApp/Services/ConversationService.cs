a128833ec12e8503af552b3529ada101d5d70f2b
G'olibjon <golibjonabdurasulov47@gmail.com>
2023-08-03 21:35:44 +0500

    Implement Conversation Service

--- TCBApp/TCBApp/Services/ConversationService.cs
+++ TCBApp/TCBApp/Services/ConversationService.cs
@@ -5,5 +5,39 @@ namespace TCBApp.Services;
 
 public class ConversationService:IConversationInterface
 {
-   
+    private DataProvider _dataProvider { get; set; }
+
+    public ConversationService(DataProvider dataProvider)
+    {
+        _dataProvider = dataProvider;
+    }
+    public ChatModel CreateConversation(ChatModel chat)
+    {
+     return  _dataProvider.CreateConversation(chat);
+    }
+
+    public ChatModel StopConversation(long chatId)
+    {
+        return _dataProvider.StopConversation(chatId);
+    }
+
+    public ChatModel GetLastConversation(long chatId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ChatModel UpdateConversation(ChatModel chat)
+    {
+        return _dataProvider.UpdateConversation(chat);
+    }
+
+    public ChatModel GetConversation(long chatId)
+    {
+        return _dataProvider.GetConversation(chatId);
+    }
+
+    public List<ChatModel> GetAllConversation()
+    {
+        return _dataProvider.GetAllConversation();
+    }
 }
