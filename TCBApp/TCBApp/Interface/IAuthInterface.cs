3d689834634c78718af16f4d2b9f8fd4f123b2d1
Aliyev Jamol <jamolxon002.iz@gmail.com>
2023-08-03 11:17:46 -0700

    Interface conflict sloved

--- TCBApp/TCBApp/Interface/IAuthInterface.cs
+++ TCBApp/TCBApp/Interface/IAuthInterface.cs
@@ -4,7 +4,7 @@ namespace TCBApp.Interface;
 
 public interface IAuthInterface
 {
-    public User Registration(long user_Id, long telgramClientId, string nickName, string password);
+        public User Registration(long user_Id, long telgramClientId, string nickName, string password);
         public Client LogIn(User user);
        
      
