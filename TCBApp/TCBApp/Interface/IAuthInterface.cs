d1f84eb1672cfc54a7d6aa4e1bc8e26d93c399ff
Aliyev Jamol <jamolxon002.iz@gmail.com>
2023-08-03 10:06:20 -0700

    Interfaces completed

--- TCBApp/TCBApp/Interface/IAuthInterface.cs
+++ TCBApp/TCBApp/Interface/IAuthInterface.cs
@@ -1,6 +1,12 @@
+using TCBApp.Models;
+
 namespace TCBApp.Interface;
 
 public interface IAuthInterface
 {
+    
+        public User CreateUser(long user_Id,string phoneNumber,long telegramClientId,string password);
+        public bool RemoveUser(long user_Id);
+        public User Finduser(long user_Id);
     
 }
--- TCBApp/TCBApp/Services/AuthService.cs
+++ TCBApp/TCBApp/Services/AuthService.cs
@@ -1,6 +1,22 @@
+using TCBApp.Interface;
+using TCBApp.Models;
+
 namespace TCBApp.Services;
 
-public class AuthService
+public class AuthService:IAuthInterface
 {
-    
+    public User CreateUser(long user_Id, string phoneNumber, long telegramClientId, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool RemoveUser(long user_Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public User Finduser(long user_Id)
+    {
+        throw new NotImplementedException();
+    }
 }
