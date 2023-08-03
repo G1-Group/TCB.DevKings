b7c16a0fc36610e23fc7e13a7a6b7a495020f0b2
Azimjon <azimabdishukurov@gmail.com>
2023-08-03 17:03:06 +0500

    BoardService is added.

--- TCBApp/TCBApp/Services/Interface/IBoardInterface.cs
+++ TCBApp/TCBApp/Services/Interface/IBoardInterface.cs
@@ -5,6 +5,9 @@ namespace TCBApp.Interface;
 
 public interface IBoardInterface
 {
-    public Task CreateBoard(string NickName,Update update, CancellationToken cancellationToken);
+    public Task CreateBoard(Update update, CancellationToken cancellationToken);
     public Task GetBoards(Update update, CancellationToken cancellationToken);
+    public  Task<Client> FindClient(long id);
+    public Task StartBoard(Update update,CancellationToken cancellationToken);
+    public Task SendMessage(Update update, CancellationToken cancellationToken, string text);
 }
