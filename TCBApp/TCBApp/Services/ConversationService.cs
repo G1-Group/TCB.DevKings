05a62f4ff18fd4a0aa09285dfb9fadfc32925f5c
G'olibjon <golibjonabdurasulov47@gmail.com>
2023-08-03 14:25:57 +0500

    conversation service implement qilindi va buglar tuzatildi

---
+++ TCBApp/TCBApp/Services/ConversationService.cs
@@ -0,0 +1,44 @@
+﻿using TCBApp.Interface;
+using TCBApp.Models;
+
+namespace TCBApp.Services;
+
+public class ConversationService:IConversationInterface
+{
+    public List<Client> VaitingUsers { get; set; }
+
+    public ChatModel GetLastConversation(long ClientId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int CheckConversationLimit(long ClientId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ChatModel CreateNewConversation(long fromClient, long toClient)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ChatModel StopConversation(long chatId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ExchangeMessage(long fromId, long toId, string message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteConversation(long chatId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<ChatModel> GetAllConversation()
+    {
+        throw new NotImplementedException();
+    }
+}
--- TCBApp/TCBApp/Services/Interface/IConversationInterface.cs
+++ TCBApp/TCBApp/Services/Interface/IConversationInterface.cs
@@ -1,6 +1,20 @@
+using TCBApp.Models;
+
 namespace TCBApp.Interface;
 
 public interface IConversationInterface
 {
-    
+  public List<Client> VaitingUsers { get; set; }
+  public ChatModel GetLastConversation(long ClientId);
+
+  public int CheckConversationLimit(long ClientId);
+
+  public ChatModel CreateNewConversation(long fromClient, long toClient);
+  public ChatModel StopConversation(long chatId);
+
+  public void ExchangeMessage(long fromId, long toId, string message);
+
+  public void DeleteConversation(long chatId);
+
+  public List<ChatModel> GetAllConversation();
 }
