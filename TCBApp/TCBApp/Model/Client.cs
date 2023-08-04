1fe5aca672df411d7bf06f68b0356b651eaeae03
G'olibjon <golibjonabdurasulov47@gmail.com>
2023-08-04 17:14:26 +0500

    changed

--- TCBApp/TCBApp/Model/Client.cs
+++ TCBApp/TCBApp/Model/Client.cs
@@ -5,7 +5,7 @@ public class Client
 {
     public long ClientId { get; set; }
     public long  UserId { get; set; }
-    public long  TelegramClientId { get; set; }
+    public string Username { get; set; }
     public string Nickname { get; set; }
     public ClientStatus Status { get; set; }
     public bool IsPremium { get; set; }
