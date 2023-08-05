6d26d333e4d46af5ded8adf4a018138c736afafb
Abdumannon Shamsiyev <0605AbMu@gmail.com>
2023-08-05 20:05:00 +0500

    implemented session manager service

--- TCBApp/TCBApp/Services/DBServices/UserDataService.cs
+++ TCBApp/TCBApp/Services/DBServices/UserDataService.cs
@@ -2,7 +2,6 @@
 using Npgsql;
 using TCBApp.Models;
 using TCBApp.Models.Enums;
-using User = Telegram.Bot.Types.User;
 
 namespace TCBApp.Services;
 
@@ -13,7 +12,8 @@ public class UserDataService:DataProvider
 
     private string selectQuery = $"SELECT * FROM {tableName}";
     private string selectByIdQuery = $"SELECT * FROM {tableName} WHERE id = @p0";
-
+    private string selectByChatIdQuery = $"SELECT * FROM {tableName} WHERE telegram_client_id= @p0";
+    
     private string insertQuery =
         $"INSERT INTO {tableName} (telegram_client_id,phone_number,password) VALUES ( @p1, @p2, @p3);";
     
@@ -49,11 +49,23 @@ public class UserDataService:DataProvider
     {
         return await this.ExecuteNonResult(this.insertQuery, new NpgsqlParameter[]
         {
-            
             new NpgsqlParameter("@p1", user.TelegramClientId),
             new NpgsqlParameter("@p2", user.PhoneNumber),
             new NpgsqlParameter("@p3", user.Password),
         });
+    }
+
+    public async Task<User?> GetUserByChatId(long chatId)
+    {
+        var reader = await this.ExecuteWithResult(this.selectByChatIdQuery, new NpgsqlParameter[]
+        {
+            new NpgsqlParameter("@p0", chatId)
+        });
+        List<Models.User> result = new List<Models.User>();
+        while (reader.Read())
+            result.Add(this.ReaderDataToModel(reader));
+
+        return result.ElementAtOrDefault(0);
     }
 
     private Models.User ReaderDataToModel(NpgsqlDataReader reader)
@@ -68,5 +80,5 @@ public class UserDataService:DataProvider
         };
     }
 
-    
+
 }
