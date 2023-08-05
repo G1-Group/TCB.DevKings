914a3246ce7f7ec0e9a111baf7006fad2e035133
G'olibjon <golibjonabdurasulov47@gmail.com>
2023-08-05 20:41:49 +0500

    created ControllerBase and HandlerAction

---
+++ TCBApp/TCBApp/ControllerBase.cs
@@ -0,0 +1,15 @@
+﻿using TCBApp.TelegramBot;
+using Telegram.Bot;
+
+namespace TCBApp;
+
+public abstract class ControllerBase
+{
+    protected readonly ITelegramBotClient _botClient;
+
+    public ControllerBase(ITelegramBotClient botClient)
+    {
+        _botClient = botClient;
+    }
+    public abstract void HandleAction(ControllerContext context);
+}
