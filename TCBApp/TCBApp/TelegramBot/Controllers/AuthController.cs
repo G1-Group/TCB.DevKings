fb2fa9f2c1006dbf066b2e7ef50e050d6ebae9fc
Aliyev Jamol <jamolxon002.iz@gmail.com>
2023-08-07 04:10:01 -0700

    AuthController Finished TO use

--- TCBApp/TCBApp/TelegramBot/Controllers/AuthController.cs
+++ TCBApp/TCBApp/TelegramBot/Controllers/AuthController.cs
@@ -7,9 +7,10 @@ namespace TCBApp.TelegramBot.Controllers;
 public class AuthController:ControllerBase
 {
     private readonly AuthService _authService;
-
+ 
     private string login = null;
-
+    private string phonenumber = null;
+    private string password;
 
     public AuthController(ITelegramBotClient botClient, AuthService authService): base(botClient)
     {
@@ -37,6 +38,36 @@ public class AuthController:ControllerBase
         });
     }
 
+    public void RegstrationUserREgistration(ControllerContext context)
+    {
+        _botClient.SendTextMessageAsync(context.Update.Message.Chat.Id, "Enter phone number as \"+998900000000\"");
+
+    }
+
+    public void RegstraionUserPhoneNumber(ControllerContext context)
+    {
+        phonenumber = context.Update.Message.Text;
+        _botClient.SendTextMessageAsync(context.Update.Message.Chat.Id, "Please Enter your password");
+        
+    }
+
+    public void Regstration(ControllerContext context)
+    {
+        password = context.Update.Message.Text;
+        _botClient.SendTextMessageAsync(context.Update.Message.Text, "You Succesfully registired");
+        _authService.Registration(new UserRegstration()
+        {
+            User = new User()
+            {
+                PhoneNumber = phonenumber,
+                Password = password,
+                TelegramClientId = context.Update.Message.Chat.Id
+            },
+            TelegramChatId = context.Update.Message.Chat.Id
+        });
+    }
+
+    
     public override void HandleAction(ControllerContext context)
     {
         if (context.Session.Action == nameof(this.LoginUserLogin))
