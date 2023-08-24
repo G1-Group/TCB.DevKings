537d6fc2896d40ea19df8d215f5bbc4c7ab572a4
Malika Rasulova <malika-rasulova-2000@mail.ru>
2023-08-24 22:25:54 +0500

    created start async metod

--- TCBApp/TCBApp.WebApi/WebHost.cs
+++ TCBApp/TCBApp.WebApi/WebHost.cs
@@ -5,10 +5,15 @@ namespace TCBApp.WebApi;
 public class WebHost
 {
     protected HttpListener _httpListener;
+    
 
     public WebHost()
     {
         _httpListener = new HttpListener();
     }
-    
+
+    public async Task StartAsync()
+    {
+        _httpListener.Start();
+    }
 }
