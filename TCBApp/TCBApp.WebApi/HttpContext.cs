7e1b34b82369285875af27b0b27c2410e25b5d64
Norov Feruzbek <norovferuzbek3963@gmail.com>
2023-08-24 22:35:34 +0300

    update

--- TCBApp/TCBApp.WebApi/HttpContext.cs
+++ TCBApp/TCBApp.WebApi/HttpContext.cs
@@ -1,11 +1,13 @@
+
+
 using System.Net;
 
 namespace TCBApp.WebApi;
 
 public class HttpContext
 {
-    public WebRequest Request { get; set; }
-    public WebResponse Response { get; set; }
+    public HttpListener Request { get; set; }
+    public HttpListener Response { get; set; }
     public string QueryString { get; set; }
     public Dictionary<string,string> Params { get; set; }
    
