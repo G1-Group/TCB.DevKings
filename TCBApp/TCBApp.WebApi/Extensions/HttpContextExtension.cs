08213bf4747ce3d2c845a54eace5f07e86aaea36
ZUXRIDDIN <zuxriddinzarifov25@gmail.com>
2023-08-24 23:33:58 +0500

    Created HttpContextExtension metod ConvertByModel

---
+++ TCBApp/TCBApp.WebApi/Extensions/HttpContextExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace TCBApp.WebApi.Extensions;
+
+public static class HttpContextExtension
+{
+    public static async Task<T> ConvertByModel<T>(this HttpContent content,HttpWebRequest requestBody)
+    {
+        using (StreamReader reader = new StreamReader(content.ReadAsStream()))
+        {
+            string json = await reader.ReadToEndAsync();
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
