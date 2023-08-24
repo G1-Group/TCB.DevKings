8c9358c97e847f274c5628e8f5de20fd693ad06a
Norov Feruzbek <norovferuzbek3963@gmail.com>
2023-08-24 22:49:50 +0300

    HttpListener typlaridan foydalandi

--- TCBApp/TCBApp.WebApi/HttpContext.cs
+++ TCBApp/TCBApp.WebApi/HttpContext.cs
@@ -6,8 +6,8 @@ namespace TCBApp.WebApi;
 
 public class HttpContext
 {
-    public HttpListener Request { get; set; }
-    public HttpListener Response { get; set; }
+    public HttpListenerRequest Request { get; set; }
+    public HttpListenerResponse Response { get; set; }
     public string QueryString { get; set; }
     public Dictionary<string,string> Params { get; set; }
    
