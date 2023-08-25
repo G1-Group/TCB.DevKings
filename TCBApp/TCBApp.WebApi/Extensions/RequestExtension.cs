97b8cfc5a9fdd290a52d03e6c314377c0e669563
ZUXRIDDIN <zuxriddinzarifov25@gmail.com>
2023-08-25 22:25:25 +0500

    created convert metod request and response

---
+++ TCBApp/TCBApp.WebApi/Extensions/RequestExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace TCBApp.WebApi.Extensions;
+
+public static class RequestExtension
+{
+    public static async Task<T> ConvertAsync<T>(this HttpListenerRequest request)
+    {
+        using (StreamReader reader = new StreamReader(request.InputStream))
+        {
+            string json = await reader.ReadToEndAsync();
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
---
+++ TCBApp/TCBApp.WebApi/Extensions/ResponseExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace TCBApp.WebApi.Extensions;
+
+public static class ResponseExtensions
+{
+    public static async Task<T?> ConvertAsync<T>(this HttpListenerResponse response )
+    {
+        
+        using (StreamReader reader = new StreamReader(response.OutputStream))
+        {
+            string json = await reader.ReadToEndAsync();
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
