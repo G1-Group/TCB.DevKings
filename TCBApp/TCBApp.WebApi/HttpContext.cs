d39fe71a81009f0db8ca6484ffa5fa3f2948172d
Abdumannon Shamsiyev <0605AbMu@gmail.com>
2023-08-25 21:50:15 +0500

    Request pipeline implemented

--- TCBApp/TCBApp.WebApi/HttpContext.cs
+++ TCBApp/TCBApp.WebApi/HttpContext.cs
@@ -1,6 +1,6 @@
-
-
+using System.Collections.Specialized;
 using System.Net;
+using System.Security.Principal;
 
 namespace TCBApp.WebApi;
 
@@ -8,7 +8,14 @@ public class HttpContext
 {
     public HttpListenerRequest Request { get; set; }
     public HttpListenerResponse Response { get; set; }
-    public string QueryString { get; set; }
-    public Dictionary<string,string> Params { get; set; }
-   
+    public NameValueCollection QueryString => Request.QueryString;
+    public IPrincipal? User { get; set; }
+
+    public static HttpContext
+        FromListenerContext(HttpListenerContext context) => new HttpContext()
+    {
+        Request = context.Request,
+        Response = context.Response,
+        User = context.User
+    };
 }
--- TCBApp/TCBApp.WebApi/WebHost.cs
+++ TCBApp/TCBApp.WebApi/WebHost.cs
@@ -1,4 +1,6 @@
 using System.Net;
+using TCBApp.WebApi.Interfaces;
+using TCBApp.WebApi.Types;
 
 namespace TCBApp.WebApi;
 
@@ -6,9 +8,59 @@ public class WebHost
 {
     protected HttpListener _httpListener;
 
+    private LinkedList<Func<HttpContext, NextHandlerDelegate, Task>> middlewares;
+    private List<IMiddleware> MiddlewareCollections = new List<IMiddleware>();
+
     public WebHost()
     {
+        middlewares = new LinkedList<Func<HttpContext, NextHandlerDelegate, Task>>();
+    }
+
+
+    public void Initialize(string prefix)
+    {
         _httpListener = new HttpListener();
+        _httpListener.Prefixes.Add(prefix);
+    }
+
+    public async Task<NextHandlerDelegate> MakeNextHandlerChainingAsync(HttpContext context,
+        LinkedListNode<Func<HttpContext, NextHandlerDelegate, Task>>? middleware)
+    {
+        if (middleware is null)
+            return null;
+
+        return async () =>
+            await middleware.Value(context,
+                await MakeNextHandlerChainingAsync(context, middleware.Next)
+            );
+    }
+
+    public void Use<T>() where T:IMiddleware
+    {
+        var middleware = Activator.CreateInstance<T>();
+        this.MiddlewareCollections.Add(middleware);
+        middlewares.AddLast(middleware.ExecuteAsync);
     }
     
+    public void Use(Func<HttpContext, NextHandlerDelegate, Task> middleware)
+    {
+        middlewares.AddLast(middleware);
+    }
+
+    private async Task AcceptContextAsync()
+    {
+        while (true)
+        {
+            var context = await _httpListener.GetContextAsync();
+            var methodChain = MakeNextHandlerChainingAsync(HttpContext.FromListenerContext(context), middlewares.First).Result;
+            ThreadPool.QueueUserWorkItem((state) => { methodChain().Wait(); });
+        }
+    }
+
+    public async Task StartAsync()
+    {
+        _httpListener.Start();
+        Console.WriteLine("Server is started on {0}.", _httpListener.Prefixes.ElementAt(0));
+        await this.AcceptContextAsync();
+    }
 }
