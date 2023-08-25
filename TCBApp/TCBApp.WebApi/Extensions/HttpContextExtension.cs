76fe2c4dac0775cc21e1c7152b3416cc584b2cac
ZUXRIDDIN <zuxriddinzarifov25@gmail.com>
2023-08-25 19:51:12 +0500

    convert to model response and reques

--- TCBApp/TCBApp.WebApi/Extensions/HttpContextExtension.cs
+++ TCBApp/TCBApp.WebApi/Extensions/HttpContextExtension.cs
@@ -5,9 +5,17 @@ namespace TCBApp.WebApi.Extensions;
 
 public static class HttpContextExtension
 {
-    public static async Task<T> ConvertByModel<T>(this HttpContext context)
+    public static async Task<T> ConvertByModelRerques<T>(this HttpContext context,HttpListenerRequest request)
     {
-        using (StreamReader reader = new StreamReader(context.Request.InputStream))
+        using (StreamReader reader = new StreamReader(request.InputStream))
+        {
+            string json = await reader.ReadToEndAsync();
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+    public static async Task<T> ConvertByModelResponse<T>(this HttpContext context,HttpListenerResponse response)
+    {
+        using (StreamReader reader = new StreamReader(response.OutputStream))
         {
             string json = await reader.ReadToEndAsync();
             return JsonSerializer.Deserialize<T>(json);
