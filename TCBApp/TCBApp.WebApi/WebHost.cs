f535ad2b7e3d01d471415da2cc362f11617b8569
Aliyev Jamol <jamolxon002.iz@gmail.com>
2023-08-25 07:59:39 -0700

    Added to webhost collection of controler base

--- TCBApp/TCBApp.WebApi/WebHost.cs
+++ TCBApp/TCBApp.WebApi/WebHost.cs
@@ -5,9 +5,11 @@ namespace TCBApp.WebApi;
 public class WebHost
 {
     protected HttpListener _httpListener;
+    protected List<ControllerBase> _controllers { get; set; }
 
     public WebHost()
     {
+        _controllers = new List<ControllerBase>();
         _httpListener = new HttpListener();
     }
     
