9bd462715ea4b218bedab9e5d643b0705fcabdda
Developer <golibjonabdurasulov47@gmail.com>
2023-08-26 08:23:19 -0700

    LogMiddleware implemented

--- TCBApp/TCBApp.WebApi/Built-in Middlewares/LogMiddleware.cs
+++ TCBApp/TCBApp.WebApi/Built-in Middlewares/LogMiddleware.cs
@@ -10,7 +10,7 @@ public class LogMiddleware : IMiddleware
     public async Task ExecuteAsync(HttpContext context, NextHandlerDelegate? next)
     {
         Console.WriteLine($"Ip: {context.Request.RemoteEndPoint} Request: {context.QueryString.ToString()}   " +
-                          $"Response status code: {context.Response.StatusCode} Time: {DateTime.Now}  Method: {next.Method}");
+                          $"Response status code: {context.Response.StatusCode}  Method: {next.Method} Time: {DateTime.Now}");
     }
     
 }
